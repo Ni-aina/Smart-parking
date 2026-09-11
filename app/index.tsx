f832b02eabb0fe4726465a8c94d7040441d51ef1
ni-aina <niaina.dev.soft@gmail.com>
2026-09-11 17:30:35 +0300

    Issues/Index responsive screen

--- app/index.tsx
+++ app/index.tsx
@@ -21,47 +21,49 @@ const AppScreen = () => {
 
     return (
         <View style={styles.container}>
-            <Pressable 
-                style={({ pressed }) => [styles.skipContent, pressed && styles.pressed]} 
-                onPress={() => router.navigate("/home")}
-            >
-                <Text style={[styles.skipText, { color: Colors[colorScheme].tint }]}>
-                    {t("skip")}
-                </Text>
-            </Pressable>
-            <Carousel
-                ref={carouselRef}
-                width={width}
-                height={height * 0.60}
-                data={sliders as sliderType[]}
-                renderItem={({ item }) => <SliderRender item={item} />}
-                onProgressChange={(_, absoluteProgress) => {
-                    if (absoluteProgress > 2.5) return setCurrentIndex(0);
-                    const index = Math.round(absoluteProgress);
-                    setCurrentIndex(index);
-                }}
-                scrollAnimationDuration={1000}
-                autoPlayInterval={3000}
-                autoPlay
-            />
-            <View style={styles.bullets}>
-                {
-                    sliders.map((_, index) => (
-                        <View
-                            key={index}
-                            style={[styles.bullet, {
-                                backgroundColor: currentIndex === index ?
-                                    Colors[colorScheme].tint :
-                                    Colors[colorScheme].icon
-                            }]}
-                        />
-                    ))
-                }
-            </View>
-            <View style={styles.buttonStyle}>
+            <View style={styles.content}>
+                <View style={styles.skipContent}>
+                    <Pressable
+                        style={({ pressed }) => [pressed && styles.pressed]}
+                        onPress={() => router.navigate("/home")}
+                    >
+                        <Text style={[styles.skipText, { color: Colors[colorScheme].tint }]}>
+                            {t("skip")}
+                        </Text>
+                    </Pressable>
+                </View>
+                <Carousel
+                    ref={carouselRef}
+                    width={width}
+                    height={height * 0.60}
+                    data={sliders as sliderType[]}
+                    renderItem={({ item }) => <SliderRender item={item} />}
+                    onProgressChange={(_, absoluteProgress) => {
+                        if (absoluteProgress > 2.5) return setCurrentIndex(0);
+                        const index = Math.round(absoluteProgress);
+                        setCurrentIndex(index);
+                    }}
+                    scrollAnimationDuration={1000}
+                    autoPlayInterval={3000}
+                    autoPlay
+                />
+                <View style={styles.bullets}>
+                    {
+                        sliders.map((_, index) => (
+                            <View
+                                key={index}
+                                style={[styles.bullet, {
+                                    backgroundColor: currentIndex === index ?
+                                        Colors[colorScheme].tint :
+                                        Colors[colorScheme].icon
+                                }]}
+                            />
+                        ))
+                    }
+                </View>
                 <Button
                     title={t("next")}
-                    onPress={() =>{
+                    onPress={() => {
                         if (currentIndex === sliders.length - 1)
                             return router.navigate("/home");
                         carouselRef.current?.next();
@@ -76,34 +78,36 @@ const AppScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    content: {
+        flex: 1,
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: 30
+    },
+    skipContent: {
+        alignSelf: "stretch",
+        alignItems: "flex-end",
+        marginRight: 30
     },
     pressed: {
         opacity: 0.7
     },
-    skipContent: {
-        position: 'absolute',
-        top: 50,
-        right: 30,
-        padding: 10
-    },
     skipText: {
         fontSize: 20,
-        fontWeight: 'bold'
+        fontWeight: "bold"
     },
     bullets: {
-        flexDirection: "row", 
+        flexDirection: "row",
+        marginBottom: 10,
         gap: 10
     },
     bullet: {
         width: 10,
         height: 10,
         borderRadius: 80
-    },
-    buttonStyle: {
-        position: 'absolute',
-        bottom: 70
     }
 })
 
